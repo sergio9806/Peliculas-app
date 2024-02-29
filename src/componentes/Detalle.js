ec69238ebfc2a2de4aa1307584f549df2867f7f8
Sergio <sergiomora31@gmail.com>
2024-02-29 16:49:45 -0600

    cambios en principal y detalle

--- src/componentes/Detalle.js
+++ src/componentes/Detalle.js
@@ -6,167 +6,107 @@ import { useNavigate } from "react-router-dom";
 import "./css/Detalles.css"
 
 const Detalle = () => {
-  const API_URL = 'https://api.themoviedb.org/3'
-  const API_KEY = '754424d785d8b491bc43ee5e13bb1a55'
-  const IMAGE_PATH = 'https://image.tmdb.org/t/p/original'
-  const URL_IMAGE = 'https://image.tmdb.org/t/p/original'
-
-  //variables de estado 
-  const navigate = useNavigate();
-  const [movies, setMovies] = useState([])
-  const [searchKey, setSearchKey] = useState("")
-  const [movie, setMovie] = useState({ title: "loading page" })
-  const [playing, setPlaying] = useState(false)
-  const [trailer, setTrailer] = useState(null)
-
-
-  //const [selectedMovie, setSelectedMovie] = useState({})
-  const navegar = () => {
-
-    navigate("/Detalle");
-  }
-  // funcion para realizar la peticion get a la api
-  const fetchMovies = async (searchKey) => {
-    const type = searchKey ? "search" : "discover";
-    const {
-      data: { results },
-    } = await axios.get(`${API_URL}/${type}/movie`, {
-      params: {
-        api_key: API_KEY,
-        query: searchKey,
-      },
-    });
-    //console.log('data',results);
-    //setSelectedMovie(results[0])
-
-    setMovies(results);
-    setMovie(results[0]);
-
-    if (results.length) {
-      await fetchMovie(results[0].id);
-    }
-  };
-
-  // funcion para la peticion de un solo objeto y mostrar en reproductor de videos
-  const fetchMovie = async (id) => {
-    const { data } = await axios.get(`${API_URL}/movie/${id}`, {
-      params: {
-        api_key: API_KEY,
-        append_to_response: "videos",
-      },
-    });
-
-    if (data.videos && data.videos.results) {
-      const trailer = data.videos.results.find(
-        (vid) => vid.name === "Official Trailer"
-      );
-      setTrailer(trailer ? trailer : data.videos.results[0]);
-    }
-    //return data
-    setMovie(data);
-  };
-
-
-
-  const selectMovie = async (movie) => {
-    // const data = await fetchMovie(movie.id)
-    // console.log(data);
-    // setSelectedMovie(movie)
-    fetchMovie(movie.id);
-
-    setMovie(movie);
-    window.scrollTo(0, 0);
-  };
-
-  // funcion para buscar peliculas
-  const searchMovies = (e) => {
-    e.preventDefault();
-    fetchMovies(searchKey);
-  };
+  const API_URL = process.env.REACT_APP_API_URL;
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const IMAGE_PATH = process.env.REACT_APP_IMAGE_PATH;
+  const URL_IMAGE = process.env.REACT_APP_URL_IMAGE;
+  const { movieId } = useParams();
+  const [cast, setCast] = useState([]);
+  const [movie, setMovie] = useState(null);
+  const [trailer, setTrailer] = useState(null);
+  const [playing, setPlaying] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    const fetchMovie = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/movie/${movieId}`, {
+          params: {
+            api_key: API_KEY,
+            append_to_response: 'videos,credits',
+          },
+        });
+        const movieData = response.data;
+        setMovie(movieData);
+
+        if (movieData.videos && movieData.videos.results) {
+          const officialTrailer = movieData.videos.results.find(vid => vid.name === "Official Trailer");
+          setTrailer(officialTrailer ? officialTrailer : movieData.videos.results[0]);
+        }
+        setCast(response.data.credits.cast); // Establecer el elenco en el estado
+      } catch (error) {
+        console.error('Error fetching movie details:', error);
+      }
+
+    };
+
+    fetchMovie();
+  }, [movieId]);
+
+  const handlePlayTrailer = () => {
+    setPlaying(true);
+  };
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+  };
 
   return (
     <div className="Detalle__principal">
-      {/* contenedor del banner y reproductor de video  */}
-      <div>
-        <main>
-          {movie ? (
-            <div
-              className="viewtrailer"
-              style={{
-                backgroundImage: `url("${IMAGE_PATH}${movie.backdrop_path}")`,
-              }}
-            >
-              {playing ? (
-                <>
-                  <YouTube
-                    videoId={trailer.key}
-                    className="reproductor container"
-                    containerClassName={"youtube-container amru"}
-                    opts={{
-                      width: "100%",
-                      height: "100%",
-                      playerVars: {
-                        autoplay: 1,
-                        controls: 0,
-                        cc_load_policy: 0,
-                        fs: 0,
-                        iv_load_policy: 0,
-                        modestbranding: 0,
-                        rel: 0,
-                        showinfo: 0,
-                      },
-                    }}
-                  />
-                  <button onClick={() => setPlaying(false)} className="boton__cerrar">
-                    Close
-                  </button>
-                </>
-              ) : (
-                <div className="container__banner">
-                  <div className="">
-                    {trailer ? (
-                      <button
-                        className="boton"
-                        onClick={() => setPlaying(true)}
-                        type="button"
-                      >
-                        Play Trailer
-                      </button>
-                    ) : (
-                      "Sorry, no trailer available"
-                    )}
-                    
-                  </div>
-                </div>
-              )}
+      {movie && (
+        <div className="viewtrailer" style={{ backgroundImage: `url("https://image.tmdb.org/t/p/original${selectedImage ? selectedImage.file_path : movie.backdrop_path}")` }}>
+          {playing ? (
+            <>
+              <YouTube
+                videoId={trailer.key}
+                className="reproductor container"
+                containerClassName="youtube-container amru"
+                opts={{
+                  width: "100%",
+                  height: "100%",
+                  playerVars: {
+                    autoplay: 1,
+                    controls: 0,
+                    cc_load_policy: 0,
+                    fs: 0,
+                    iv_load_policy: 0,
+                    modestbranding: 0,
+                    rel: 0,
+                    showinfo: 0,
+                  },
+                }}
+              />
+              <button onClick={() => setPlaying(false)} className="boton__cerrar">Cerrar</button>
+            </>
+          ) : (
+            <div className="container__banner">
+              <div>
+                {trailer ? (
+                  <button className="boton" onClick={handlePlayTrailer} type="button">Reproducir Tráiler</button>
+                ) : (
+                  <p>Lo siento, no hay tráiler disponible</p>
+                )}
+              </div>
             </div>
-          ) : null}
-        </main>
-      </div>
-      {/* informacion sobre la pelicula  */}
-      <div className="container__informacion">
-        <div>
-           <div className="portada__pelicula" > 
-           </div>
-              <div className="informacion__pelicula">
-                     <h1 className="text-white">{movie.title}</h1>
-                       <p className="text-white">{movie.overview}</p>
-   
-             </div> 
-        
-        </div>  
-        
-      </div>
-      {/* contenedor de poster de peliculas */}
-      <div className='container__peliculas'>
-        <div className="peliculas__seccion">
-          <h3 className="peliculas__recomendadas">Peliculas recomendadas</h3>
-          <div className="indicadores_peliculas">
-            
+          )}
+        </div>
+      )}
+
+      {movie && (
+        <div className="container__informacion">
+          <div>
+            <div className="portada__pelicula"></div>
+            <div className="informacion__pelicula">
+              <h1 className="text-white">{movie.title}</h1>
+
+
+              <p className="text-white">{movie.overview}</p>
+              {/* Renderizar la información del elenco */}
+              <div className="cast__list">
+                <h3>Elenco:</h3>
+                <p>{cast.map(actor => actor.name).join(", ")}</p>
+              </div>
+            </div>
           </div>
         </div>
         <div className="container__principal">
@@ -182,6 +122,7 @@ const Detalle = () => {
              </div>
           <button role='button' id='flecha__derecha' className="flecha__derecha">{'<'}</button>
         </div>
+
       </div>
     </div>
 
